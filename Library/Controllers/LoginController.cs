ce31e525e9b9d54e51a4b7fcd3a99a61885b45cb
VdMt98 <vadim.28.matyash@gmail.com>
2017-04-22 19:20:54 +0300

    add normal loginizatino

---
+++ Library/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Views;
+using Library.Models.TableElements;
+
+namespace Library.Controllers
+{
+    class LoginController
+    {
+        public Profile worker;
+
+        public void LogIn(int ids, string password, Login log)
+        {
+            worker = DAO.GetProfileTable().GetProfileByLoginAndPassword(ids, password);
+
+            
+
+            switch (worker.profileCategory.id)
+            {
+                case 1:
+                    ReaderMainWindow rm = new ReaderMainWindow();
+                    rm.Show();
+                    break;
+                case 2:
+                    LibrarianMainWindow lm = new LibrarianMainWindow();
+                    lm.Show();
+                    break;
+                case 3:
+                    AdminMainWindow ad = new AdminMainWindow();
+                    ad.Show();
+                    break;
+            }
+            log.Close();
+        }
+    }
+}
--- Library/Views/Login.xaml.cs
+++ Library/Views/Login.xaml.cs
@@ -29,11 +29,16 @@ namespace Library.Views
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            int login = int.Parse(tbLogin.Text);
-            String password = pbPassword.Password;
-            string info = DAO.GetProfileTable().GetProfileByLoginAndPassword(login, password).ToString();
-            MessageBox.Show(info);
-            
+            try
+            {
+                int login = int.Parse(tbLogin.Text);
+                String password = pbPassword.Password;
+                LoginController lg = new LoginController();
+                lg.LogIn(login, password, this);
+            }catch(FormatException)
+            {
+                MessageBox.Show("Некоректно введені дані");
+            }
         }
     }
 }
